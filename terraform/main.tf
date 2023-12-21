bbd1afd4cd2297825fb1fd259eb9afb53f9fe6d9
abhi <aparthan277@gmail.com>
2023-12-21 15:11:13 +0530

    version14

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,11 +5,14 @@
 data "aws_ami" "new_ami" {
 
   most_recent = true
-  filter {
-    tags.#.project = shopping
-    tags.#.env =  production
-  }
+  owners = ["576000108196"]
 
+  source_ami_filter {
+    filters = {
+      name   = "source-ami-id"
+      values = ["ami-02e94b011299ef128"]  # Replace with the source AMI ID you want to filter by
+    }
+  }
 }
 
 output "ami_id" {
